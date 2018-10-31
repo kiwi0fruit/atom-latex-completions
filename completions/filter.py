f0a5f9eed0b0be9dcc42f738856853a94cd1f5ea
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-10-31 19:44:08 +0700

    bugfix

--- completions/filter.py
+++ completions/filter.py
@@ -7,7 +7,7 @@ with open(p.join('.', '__completions.json'), 'r', encoding='utf-8') as f:
 numbers = dict(zero=0, one=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9)
 others = dict(ltphi=r'phi\\lt', pgamma=r'gamma\\phon', pbgam=r'gamma\\phon2', ltlmr=r'r\\ltl',
               imath=r'i\\math', jmath=r'j\\math', itimath=r'i\\itmath', itjmath=r'j\\itmath',
-              bigamma=r'gamma\\bi', big=r'g\\bi', bfiota=r'iota\\bf', upvarbeta=r'beta\\upvar'
+              bigamma=r'gamma\\bi', big=r'g\\bi', bfiota=r'iota\\bf', upvarbeta=r'beta\\upvar',
               sansLturned=r'L\\rot\\sans', sansLmirrored=r'L\\mirr\\sans')
 
 src = re.sub(f'"({"|".join(map(re.escape, others.keys()))})": "',
