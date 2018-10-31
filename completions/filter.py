75c0c051c01ac868e61723121ef09124fb6a90d8
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-10-31 18:18:01 +0700

    bugfix

--- completions/filter.py
+++ completions/filter.py
@@ -5,15 +5,15 @@ with open(p.join('.', '__completions.json'), 'r', encoding='utf-8') as f:
     src = f.read()
 
 numbers = dict(zero=0, one=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9)
+others = dict(ltphi=r'phi\\lt', pgamma=r'gamma\\phon', pbgam=r'gamma\\phon2', ltlmr=r'r\\ltl',
+              imath=r'i\\math', jmath=r'j\\math', itimath=r'i\\itmath', itjmath=r'j\\itmath')
+
+src = re.sub(f'({"|".join(map(re.escape, others.keys()))})',
+             lambda m: others.get(m.group(0)),
+             src)
 
 src = re.sub(r'"(b?frak|b?i?sans(?:var)?|[bm]?scr|it(?:var)?|b[bif]|tt|rtl|ltl)(\w+)": "',
              lambda m: rf'"{numbers.get(m.group(2), m.group(2))}\\{m.group(1)}": "',
-             src.replace('"ltphi"', r'"phi\\lt"'
-                         ).replace('"pgamma"', r'"gamma\\phon"'
-                                   ).replace('"pbgam"', r'"gamma\\phon2"'
-                                             ).replace('"ltlmr"', r'"r\\ltl"'))
-src = re.sub(r'"(it)?(\w+?)math": "',
-             lambda m: rf'"{m.group(2)}\\{m.group(1) if m.group(1) else ""}math": "',
              src)
 
 with open(p.join('.', 'completions.json'), 'w', encoding='utf-8') as f:
