67e344d1bec35dcad0d8e068d14455dadc878536
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-10-31 18:33:53 +0700

    bugfix

--- completions/filter.py
+++ completions/filter.py
@@ -6,15 +6,17 @@ with open(p.join('.', '__completions.json'), 'r', encoding='utf-8') as f:
 
 numbers = dict(zero=0, one=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9)
 others = dict(ltphi=r'phi\\lt', pgamma=r'gamma\\phon', pbgam=r'gamma\\phon2', ltlmr=r'r\\ltl',
-              imath=r'i\\math', jmath=r'j\\math', itimath=r'i\\itmath', itjmath=r'j\\itmath')
+              imath=r'i\\math', jmath=r'j\\math', itimath=r'i\\itmath', itjmath=r'j\\itmath',
+              bigamma=r'gamma\\bi', big=r'g\\bi')
 
 src = re.sub(f'({"|".join(map(re.escape, others.keys()))})',
              lambda m: others.get(m.group(0)),
              src)
 
-src = re.sub(r'"(b?frak|b?i?sans(?:var)?|[bm]?scr|it(?:var)?|b[bf]i?|bi(?!g)|tt|rtl|ltl)(\w+)": "',
-             lambda m: rf'"{numbers.get(m.group(2), m.group(2))}\\{m.group(1)}": "',
-             src)
+src = re.sub(
+    r'"(b?frak|b?i?sans(?:var)?|[bm]?scr|it(?:var)?|b[bf](?:i(?!ota))?|bi(?!g)|tt|rtl|ltl)(\w+)": "',
+    lambda m: rf'"{numbers.get(m.group(2), m.group(2))}\\{m.group(1)}": "',
+    src)
 
 with open(p.join('.', 'completions.json'), 'w', encoding='utf-8') as f:
     f.write(src)
