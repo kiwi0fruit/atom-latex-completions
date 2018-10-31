cec734c2f4c7e4e40e3ac40a65ab5ce0890d3fdd
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-10-31 18:07:23 +0700

    bugfix

--- completions/filter.py
+++ completions/filter.py
@@ -6,13 +6,13 @@ with open(p.join('.', '__completions.json'), 'r', encoding='utf-8') as f:
 
 numbers = dict(zero=0, one=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9)
 
-src = re.sub(r'"(b?frak|b?i?sans|[bm]?scr|itvar|it|b[bif]|tt|rtl|ltl)(\w+)": "',
+src = re.sub(r'"(b?frak|b?i?sans(?:var)?|[bm]?scr|it(?:var)?|b[bif]|tt|rtl|ltl)(\w+)": "',
              lambda m: rf'"{numbers.get(m.group(2), m.group(2))}\\{m.group(1)}": "',
              src.replace('"ltphi"', r'"phi\\lt"'
                          ).replace('"pgamma"', r'"gamma\\phon"'
                                    ).replace('"pbgam"', r'"gamma\\phon2"'
                                              ).replace('"ltlmr"', r'"r\\ltl"'))
-src = re.sub(r'"(it)?(\w+)math": "',
+src = re.sub(r'"(it)?(\w+?)math": "',
              lambda m: rf'"{m.group(2)}\\{m.group(1) if m.group(1) else ""}math": "',
              src)
 
